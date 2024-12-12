7053305250e572644cc6a4b33d887fb4f0f303ed
ngockha <sury240501@gmail.com>
2024-12-13 02:43:21 +0700

    refactor: update method payment

--- component/screen/Resident/Payment/PaymentDetail.js
+++ component/screen/Resident/Payment/PaymentDetail.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Pressable, Animated, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, Pressable, Animated, ScrollView, Alert } from 'react-native';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { styles } from '@component/styles/paymentDetailStyles';
 
@@ -22,6 +22,30 @@ const PaymentDetail = ({ navigation, route }) => {
     }).start();
   };
 
+  // Hàm xử lý khi nhấn nút thanh toán
+  const handlePayment = () => {
+    if (selectedMethod === 'counter') {
+      Alert.alert(
+        "Thông báo",
+        "Mời bạn đến quầy lễ tân của Vinhomes Serenity tại HCM để thanh toán.",
+        [{ text: "OK"}]
+      );
+    } else if (selectedMethod === 'onlineBanking') {
+      Alert.alert(
+        "Thông báo",
+        "Chức năng này chưa được hỗ trợ",
+        [{ text: "OK" }]
+      );
+      
+    } else if (selectedMethod === 'onlineApp') {
+      Alert.alert(
+        "Thông báo",
+        "Thanh toán thành công!",
+        [{ text: "OK", onPress: () => navigation.navigate("Invoice") }]
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* Header */}
@@ -35,6 +59,7 @@ const PaymentDetail = ({ navigation, route }) => {
       <ScrollView style={styles.content}>
         <Text style={styles.title}>Phương thức thanh toán</Text>
 
+        {/* Thanh toán tại quầy lễ tân */}
         <TouchableOpacity
           style={styles.optionContainer}
           onPress={() => setSelectedMethod('counter')}
@@ -51,9 +76,9 @@ const PaymentDetail = ({ navigation, route }) => {
               trạng thái thanh toán.  
             </Text>
           </View>
-          
         </TouchableOpacity>
 
+        {/* Chuyển khoản ngân hàng trực tuyến */}
         <TouchableOpacity
           style={styles.optionContainer}
           onPress={() => setSelectedMethod('onlineBanking')}
@@ -67,25 +92,38 @@ const PaymentDetail = ({ navigation, route }) => {
               Chuyển khoản ngân hàng trực tuyến qua {"\n"} ứng dụng VNPay
             </Text>
           </View>
-          
+        </TouchableOpacity>
+
+        {/* Thanh toán trực tiếp tại app */}
+        <TouchableOpacity
+          style={styles.optionContainer}
+          onPress={() => setSelectedMethod('onlineApp')}
+          onPressIn={handlePressIn}
+          onPressOut={handlePressOut}
+        > 
+          <View style={[styles.radioCircle, selectedMethod === 'onlineApp' && styles.radioSelected]} />
+          <View style={styles.radioContainer}>          
+            <Text style={styles.optionTitle}>Chuyển khoản trực tiếp</Text>
+            <Text style={styles.optionText}>
+              Chuyển khoản trực tiếp tại app 
+            </Text>
+          </View>
         </TouchableOpacity>
       </ScrollView>
 
+      {/* Tổng tiền và nút thanh toán */}
       <View style={styles.transactionSummaryContainer}>
         <View style={styles.transactionSummary}>
           <Text style={styles.transactionText}>Thành tiền</Text>
           <Text style={styles.transactionAmount}>{totalAmount.toLocaleString("vi-VN")} VNĐ</Text>
         </View>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handlePayment}>
           <Text style={styles.buttonText}>Thanh toán</Text>
         </TouchableOpacity>
       </View>
-
-      
     </View>
   );
 };
 
 export default PaymentDetail;
-
