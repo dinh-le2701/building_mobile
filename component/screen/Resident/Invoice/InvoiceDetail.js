2dd33f639f0a96ea19dbb5eb46a10e89b5b59b33
ngockha <sury240501@gmail.com>
2024-12-13 02:21:03 +0700

    refactor: update detail infomation invoice

--- component/screen/Resident/Invoice/InvoiceDetail.js
+++ component/screen/Resident/Invoice/InvoiceDetail.js
@@ -7,9 +7,16 @@ const InvoiceDetail = ({ route, navigation }) => {
   const { 
     electricityAmount,
     waterAmount,
+    hygieneFee,
     managementFee,
     totalAmount,
     billingMonth,
+    electricityOld,
+    electricityNew,
+    electricityUsed,
+    waterOld,
+    waterNew,
+    waterUsed,
   } = route.params; 
 
   // Hàm xử lý khi nhấn vào nút thanh toán
@@ -20,7 +27,14 @@ const InvoiceDetail = ({ route, navigation }) => {
       billingMonth,
       electricityAmount,
       waterAmount,
+      hygieneFee,
       managementFee,
+      electricityOld,
+      electricityNew,
+      electricityUsed,
+      waterOld,
+      waterNew,
+      waterUsed,
     });
   };
 
@@ -37,28 +51,40 @@ const InvoiceDetail = ({ route, navigation }) => {
       {/* Nội dung chính được bọc trong ScrollView */}
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <View style={styles.invoiceInfo}>
-          <Text style={styles.text0}>Hóa đơn tháng {billingMonth}</Text>
+          <Text style={styles.text0}>HÓA ĐƠN THÁNG {billingMonth}</Text>
 
           {/* Tiền điện */}
           <View style={styles.itemContainer}>
-            <Text style={styles.text}>Tiền điện:</Text>
-            <Text style={styles.amountText}>{electricityAmount.toLocaleString()} đồng</Text>
+            <Text style={styles.text1}>TIỀN ĐIỆN</Text>
+            <Text style={styles.text}>Số cũ: {electricityOld}</Text>
+            <Text style={styles.text}>Số mới: {electricityNew}</Text>
+            <Text style={styles.text}>Số điện đã sử dụng: {electricityUsed}</Text>
+            <Text style={styles.amountText1}>Thành tiền: {electricityAmount.toLocaleString()} đồng</Text>
           </View>
 
           {/* Tiền nước */}
           <View style={styles.itemContainer}>
-            <Text style={styles.text}>Tiền nước:</Text>
-            <Text style={styles.amountText}>{waterAmount.toLocaleString()} đồng</Text>
+            <Text style={styles.text1}>TIỀN NƯỚC</Text>
+            <Text style={styles.text}>Số cũ: {waterOld}</Text>
+            <Text style={styles.text}>Số mới: {waterNew}</Text>
+            <Text style={styles.text}>Số nước đã sử dụng: {waterUsed}</Text>
+            <Text style={styles.amountText1}>Thành tiền: {waterAmount.toLocaleString()} đồng</Text>
+          </View>
+
+            {/* Phí vệ sinh */}
+          <View style={styles.itemContainer}>
+            <Text style={styles.text1}>PHÍ VỆ SINH</Text>
+            <Text style={styles.amountText1}>Đơn giá: {hygieneFee.toLocaleString()} đồng</Text>
           </View>
 
           {/* Phí quản lý */}
           <View style={styles.itemContainer}>
-            <Text style={styles.text}>Phí quản lý:</Text>
-            <Text style={styles.amountText}>{managementFee.toLocaleString()} đồng</Text>
+            <Text style={styles.text1}>PHÍ QUẢN LÝ</Text>
+            <Text style={styles.amountText1}>Đơn giá: {managementFee.toLocaleString()} đồng</Text>
           </View>
 
           {/* Tổng tiền */}
-          <View style={styles.itemContainer}>
+          <View style={styles.itemContainer2}>
             <Text style={styles.text2}>Tổng tiền:</Text>
             <Text style={styles.amountText2}>{totalAmount.toLocaleString()} đồng</Text>
           </View>
